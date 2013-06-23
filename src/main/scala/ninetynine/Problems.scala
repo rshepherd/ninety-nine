131d7daaf7b1aefcebb3080b33adcbfaccbafaa8
rshepherd <me@rshepherd.org>
2013-06-23 00:12:07 -0400

    Problems 4 & 5

--- src/main/scala/ninetynine/Problems.scala
+++ src/main/scala/ninetynine/Problems.scala
@@ -2,8 +2,8 @@ package ninetynine
 
 object Problems {
 
-  def main(args: Array[String]) = println("If you're having state problems, I feel bad for you son, " +
-                                          "I've got 99 problems but side-effects ain't one.")
+  def main(args: Array[String]) = println("If you're having side-effect problems I feel bad for you son, " +
+                                          "I've got 99 problems but mutation ain't one.")
 
   // P01 (*) Find the last element of a list.
   def last[A](list: List[A]) : A = list.last
@@ -14,6 +14,11 @@ object Problems {
   // P03 (*) Find the Kth element of a list.
   def nth[A](i: Int, list: List[A]) : A = list.drop(i).head
 
+  // P04 (*) Find the number of elements of a list.
+  def len[A](list: List[A]) = list.foldLeft(0) { (a, _) => a + 1 }
+
+  // P05 (*) Reverse a list.
+  def rev[A](list: List[A]): List[A] = list.foldLeft(List[A]()) { (list, h) => h :: list }
 }
 
 
--- src/test/scala/ninetynine/ProblemsSpec.scala
+++ src/test/scala/ninetynine/ProblemsSpec.scala
@@ -5,7 +5,7 @@ import org.specs2.mutable._
 
 class ProblemsSpec extends Specification {
 
-  "My solution should" should {
+  "My solution" should {
 
     "find the last element of a list. (P01)" in {
       last(List(1, 1, 2, 3, 5, 8)) must_== 8
@@ -17,6 +17,14 @@ class ProblemsSpec extends Specification {
 
     "find the Kth element of a list. (P03)" in {
       nth(2, List(1, 1, 2, 3, 5, 8)) must_== 2
+    }
+
+    "find the number of elements of a list. (P04)" in {
+      len(List(1, 1, 2, 3, 5, 8)) must_== 6
+    }
+
+    "reverse a list. (P05)" in {
+      rev(List(1, 1, 2, 3, 5, 8)) must_== List(8, 5, 3, 2, 1, 1)
     }
 
   }
