9a8c60988376405c4ae44e11f01e4dba972537ca
Randy Shepherd <randy@tapad.com>
2013-06-23 16:45:21 -0400

    Problems 6, 7 and 8

--- src/main/scala/ninetynine/Problems.scala
+++ src/main/scala/ninetynine/Problems.scala
@@ -6,19 +6,41 @@ object Problems {
                                           "I've got 99 problems but mutation ain't one.")
 
   // P01 (*) Find the last element of a list.
-  def last[A](list: List[A]) : A = list.last
+  def last[A](list: List[A]) = list.last
 
   // P02 (*) Find the last but one element of a list.
-  def penultimate[A](list: List[A]) : A = list.dropRight(1).last
+  def penultimate[A](list: List[A]) = list.dropRight(1).last
 
   // P03 (*) Find the Kth element of a list.
-  def nth[A](i: Int, list: List[A]) : A = list.drop(i).head
+  def nth[A](i: Int, list: List[A]) = list.drop(i).head
 
   // P04 (*) Find the number of elements of a list.
   def len[A](list: List[A]) = list.foldLeft(0) { (a, _) => a + 1 }
 
   // P05 (*) Reverse a list.
-  def rev[A](list: List[A]): List[A] = list.foldLeft(List[A]()) { (list, h) => h :: list }
+  def rev[A](list: List[A]) = list.foldLeft(List[A]()) { (list, h) => h :: list }
+
+  // P06 (*) Find out whether a list is a palindrome.
+  def palindrome[A](list: List[A]) = {
+    def compare(l1: List[A], l2: List[A]): Boolean = {
+      if (l1.isEmpty) true
+      else if (l1.head != l2.head) false
+      else compare(l1.tail, l2.tail)
+    }
+    compare(list, list.reverse)
+  }
+
+  // P07 (**) Flatten a nested list structure.
+  def flatten(lists: List[Any]) : List[Any] = lists.flatMap {
+    case l: List[Any] => flatten(l)
+    case a: Any => List(a)
+  }
+
+  // P08 (**) Eliminate consecutive duplicates of list elements.
+  def compress[A](list: List[A]) = list.foldLeft(List[A]()) { (l, e) =>
+    if (l.isEmpty || l.head != e) e :: l  else l
+  }.reverse
+
 }
 
 
--- src/test/scala/ninetynine/ProblemsSpec.scala
+++ src/test/scala/ninetynine/ProblemsSpec.scala
@@ -27,6 +27,20 @@ class ProblemsSpec extends Specification {
       rev(List(1, 1, 2, 3, 5, 8)) must_== List(8, 5, 3, 2, 1, 1)
     }
 
+    "find out if a list is a palindrome. (P06)" in {
+      palindrome(List(1, 2, 3, 2, 1)) must beTrue
+      palindrome(List(1, 2, 2, 1)) must beTrue
+      palindrome(List(1, 2, 2, 1, 2)) must beFalse
+    }
+
+    "flatten a nested structure. (P07)" in {
+      flatten(List(List(1, 1), 2, List(3, List(5, 8)))) must_== List(1, 1, 2, 3, 5, 8)
+    }
+
+    "eliminate consecutive duplicates of list elements. (P08)" in {
+      compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) must_== List('a, 'b, 'c, 'a, 'd, 'e)
+    }
+
   }
 
 }
